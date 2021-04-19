4899e47bcea7b99639cdd0657a5a34fe32fb05a3
Milena <milelucero_09@hotmail.com>
2021-04-19 19:11:56 -0300

    Revert "Created Slider feature"
    This reverts commit 428f05c7d188c8038b18f2282fd60f3f04af3ab6.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import InputSlider from '../components/Slider';
-
 export default function Home(){
     return(
         <div>
-            <InputSlider label={'Label'}/>
+            <h1>Home</h1>
         </div>
     )
 }
