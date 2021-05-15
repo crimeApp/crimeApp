f882a8e2a29b2283d99e264939bcd57cc055bda8
MilenaL2018 <milelucero_09@hotmail.com>
2021-05-15 19:56:57 -0300

    Deleted comments

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,6 @@
 import { React } from 'react';
 import './Select.css';
 
-/* 
-xs, sm, md , lg , xl ==> con un grid para configurar el tamanio
-className, style ==> para editar los estilos
-value, onChange ==> para cambiar los valores
-ref ==> por si usamos useForm
-name ==> para ponerle un nombre al elemento y evento
-id o key ==> para identificarlo
-color o type para tener varias versiones en uno solo
-label, placeholder, text, error, text_error para mostrar en pantalla
-icon por si tiene iconos
-*/ 
 const Select = ({ label, options, value, handleChange, error, size, spacing  }) => {
     return (
         <div className={`select-container ${size}`}>
